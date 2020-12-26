fd0efa24a8cbbebab1f79cdf5ae6bf15240d3804
Harkeerat Singh <63100973+harkred@users.noreply.github.com>
2020-12-26 22:31:59 +0530

    Fixed th problem
    The code day4p2.py is finally workin

--- day4p2.py
+++ day4p2.py
@@ -1,17 +1,17 @@
 '''
-byr (Birth Year) - four digits; at least 1920 and at most 2002.
-iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-hgt (Height) - a number followed by either cm or in:
-If cm, the number must be at least 150 and at most 193.
-If in, the number must be at least 59 and at most 76.
-hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-pid (Passport ID) - a nine-digit number, including leading zeroes.
-cid (Country ID) - ignored, missing or not.
+1. byr (Birth Year) - four digits; at least 1920 and at most 2002.
+2. iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+3. eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+4. hgt (Height) - a number followed by either cm or in:
+   If cm, the number must be at least 150 and at most 193.
+   If in, the number must be at least 59 and at most 76.
+5. hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+6. ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+7. pid (Passport ID) - a nine-digit number, including leading zeroes.
+8. cid (Country ID) - ignored, missing or not.
 '''
 def checker(wrd):
-    a='ghijklmnopqrstuvwxyz'
+    a='ghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for i in list(a):
         if i in wrd:
             return 0
@@ -37,49 +37,41 @@ with open('input4.txt', 'r') as f:
             nlst.extend(word.split())
         ulst.append(nlst)
         nlst=[]
-    
+
     #Updated
-    cou=0
+    cou=[]
     for data in ulst:
         for i in data:
         
             if 'byr' in i:
-                if int(i[4:])>=1920 and int(i[4:])<=2002:
-                    cou+=1
+                if int(i[4:])>=1920 and int(i[4:])<=2002: cou.append('byr')
                     
             elif 'iyr' in i:
-                if int(i[4:])>=2010 and int(i[4:])<=2020:
-                    cou+=1
+                if int(i[4:])>=2010 and int(i[4:])<=2020: cou.append('iyr')
                     
             elif 'eyr' in i:
-                if int(i[4:])>=2020 and int(i[4:])<=2030:
-                    cou+=1
+                if int(i[4:])>=2020 and int(i[4:])<=2030: cou.append('eyr')
                     
             elif 'hgt' in i:
-                if 'cm'==i[len(i)-3:len(i)]:
-                    if int(i[4:].rstrip('cm'))<=150 and int(i[4:].rstrip('cm'))>=193:
-                        cou+=1
+                if 'cm' in i:
+                    if int(i[4:].rstrip('cm'))>=150 and int(i[4:].rstrip('cm'))<=193: cou.append('hgt')
                         
                 elif 'in'==i[len(i)-3:len(i)]:
-                    if int(i[4:].rstrip('in'))<=59 and int(i[4:].rstrip('in'))>=76:
-                        cou+=1
+                    if int(i[4:].rstrip('in'))>=59 and int(i[4:].rstrip('in'))<=76: cou.append('hgt')
                         
             elif 'hcl' in i:
                 if '#' in i[4:]:
-                    if len(i[4:].lstrip('#'))==6 and checker(i[4:].lstrip('#'))==1:
-                        cou+=1
+                    if len(i[4:].lstrip('#'))==6 and checker(i[4:].lstrip('#'))==1: cou.append('hcl')
             
             elif 'ecl' in i:
-                if i[4:]=='amb' or i[4:]=='blu' or i[4:]=='brn' or i[4:]=='grn' or i[4:]=='hzl' or i[4:]=='oth':
-                    cou+=1
+                if i[4:]=='amb' or i[4:]=='blu' or i[4:]=='brn' or i[4:]=='gry' or i[4:]=='grn' or i[4:]=='hzl' or i[4:]=='oth': cou.append('ecl')
                     
             elif 'pid' in i:
-                if len(i[4:])==9 and i[4:][0]=='0':
-                    cou+=1
+                if len(i[4:])==9: cou.append('pid')
 
-        if cou==7:
-            correct+=1
-        cou=0
+        if len(cou)==7: correct+=1
+        else: pass
+        cou=[]
     print(correct)
                 
         
